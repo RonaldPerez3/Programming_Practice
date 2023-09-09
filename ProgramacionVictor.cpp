dab72bd536153b66e7ecf14bacf73f4ec2ad5d3d
VictorRavelo <victor.ravelo1@upr.edu>
2023-09-08 20:31:19 -0400

    Primer trabajo de Victor para Leo y Ronald

--- ProgramacionVictor.cpp
+++ ProgramacionVictor.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
-
+//programa para hacer una calculadora basica
 int main()
 {
-    cout << "Ronald es gay" << endl;
+    char op;
+    double num1, num2;
+
+    cout << "Enter operator: +, -, *, /:";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    //Haz un codigo tipo "Switch" para cada caso y al final comprobar cuando num1=47.8 y num2=98.5 en todos los casos
+
     
+
+    return 0;
 }
