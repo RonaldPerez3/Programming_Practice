978a85f6ced1ad3509c1ec2ba2d125f18fd459b7
RonaldPerez3 <ronald.perez3@upr.edu>
2023-11-16 17:19:12 -0400

    Started chap 3 practice and organized things

---
+++ Ronald-Programs/C++ book/Chapter-3/Chap3 programs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*Program 3-1
+int main(){
+    int length, width, area;
+    
+    cout << "This program calculates the area of a rectangle.\n";
+
+    cout << "What is the length of the rectangle?" << endl;
+    cin >> length;
+
+    cout << "What is the width of the rectangel?" << endl;
+    cin >> width;
+
+    area = length * width;
+    cout << "The area of the rectangle is: " << area << endl;
+    
+    return 0;
+}*/
+
+/*Program 3-2*/
+
+int main(){
+
+    return 0;
+}
